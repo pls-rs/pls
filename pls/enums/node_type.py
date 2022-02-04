7147e8e3c8c1abdc86d038d1eb33176b88acc348
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-04 21:20:21 +0400

    Expand `NodeTypes` to a large set of unsupported types

--- pls/enums/node_type.py
+++ pls/enums/node_type.py
@@ -1,4 +1,6 @@
 from enum import auto
+from stat import S_ISBLK, S_ISCHR, S_ISDIR, S_ISFIFO, S_ISLNK, S_ISREG, S_ISSOCK
+from typing import Callable
 
 from pls.enums.base import AutoEnum
 
@@ -14,6 +16,23 @@ class NodeType(AutoEnum):
     This enum lists these possibilities.
     """
 
-    FILE = auto()
-    FOLDER = auto()
-    SYMLINK = auto()  # TODO: Handle these types of nodes
+    FOLDER = auto()  # directory
+    FILE = auto()  # regular file
+    # TODO: Handle these types of nodes
+    CHR = auto()  # character special device file
+    BLK = auto()  # block special device file
+    FIFO = auto()  # named pipe
+    LNK = auto()  # symbolic link
+    SOCK = auto()  # socket
+
+
+type_test_map: dict[NodeType, Callable[[int], bool]] = {
+    NodeType.FOLDER: S_ISDIR,
+    NodeType.FILE: S_ISREG,
+    NodeType.CHR: S_ISCHR,
+    NodeType.BLK: S_ISBLK,
+    NodeType.FIFO: S_ISFIFO,
+    NodeType.LNK: S_ISLNK,
+    NodeType.SOCK: S_ISSOCK,
+}
+"""a mapping of node types with specific functions that evaluate it"""

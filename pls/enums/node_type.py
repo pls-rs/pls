688e70551433389dd5bb12fc0aaea467910305f7
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-06 13:45:44 +0400

    Move symlinks to the top as they can point to any of the other files below

--- pls/enums/node_type.py
+++ pls/enums/node_type.py
@@ -16,12 +16,12 @@ class NodeType(AutoEnum):
     This enum lists these possibilities.
     """
 
+    SYMLINK = auto()  # symbolic link
     DIR = auto()  # directory
     FILE = auto()  # regular file
     FIFO = auto()  # named pipe
     CHAR_DEVICE = auto()  # character special device file
     BLOCK_DEVICE = auto()  # block special device file
-    SYMLINK = auto()  # symbolic link
     SOCKET = auto()  # socket
 
 

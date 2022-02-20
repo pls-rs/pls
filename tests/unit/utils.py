4317b0ff6033da93fc210ab905ad37da06ad205a
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-20 16:19:00 +0400

    Fix broken tests

---
+++ tests/unit/utils.py
@@ -0,0 +1,14 @@
+import re
+
+
+def strip_formatting(fmt_text: str) -> str:
+    """
+    Strip all Rich formatting tags out of the given string, returning only the
+    underlying plain text.
+
+    :param fmt_text: the formatted text from which to strip tags
+    :return: the text stripped of all formatting
+    """
+
+    fmt_pattern = re.compile(r"\[/?\w*]")
+    return re.sub(fmt_pattern, "", fmt_text)

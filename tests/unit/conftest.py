c547dd025407ce6dce985c77463bbca61b5b45ee
Dhruv Bhanushali <dhruv_b@live.com>
2022-03-21 01:14:42 +0400

    Refactor tests for global state

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,30 +1,13 @@
-from unittest.mock import MagicMock, patch
+from typing import Literal
 
 import pytest
 
 
-@pytest.fixture
-def pwd_grp():
-    mock_user = MagicMock(pw_name="usr1", pw_gid=0)
-    pwd = MagicMock(getpwuid=MagicMock(return_value=mock_user))
-    grp = MagicMock(
-        getgrall=MagicMock(
-            return_value=[
-                MagicMock(gr_name="grp1", gr_gid=1, gr_mem=["usr1"]),
-                MagicMock(gr_name="grp2", gr_gid=2, gr_mem=["usr2", "usr1"]),
-                MagicMock(gr_name="grp3", gr_gid=3, gr_mem=["usr2"]),
-            ]
-        )
-    )
+scope: Literal["session"] = "session"
 
-    patchers = [
-        patch.dict("sys.modules", {"pwd": pwd, "grp": grp}),
-        patch("os.getuid", MagicMock(return_value=219)),
-    ]
-    for patcher in patchers:
-        patcher.start()
 
-    yield pwd, grp
+@pytest.fixture(autouse=True, scope=scope)
+def init():
+    from pls.main import init
 
-    for patcher in patchers:
-        patcher.stop()
+    init([])  # initialise with blank arguments
--- tests/unit/globals/test_state.py
+++ tests/unit/globals/test_state.py
@@ -12,18 +12,18 @@ pytestmark = pytest.mark.skipif(
 
 
 def test_uid(pwd_grp):
-    state.parse_args([])
-    assert state.uid == 219
+    state.state.setup_user_groups()
+    assert state.state.uid == 219
 
 
 def test_gids(pwd_grp):
-    state.parse_args([])
-    assert state.gids == {0, 1, 2}
+    state.state.setup_user_groups()
+    assert state.state.gids == {0, 1, 2}
 
 
 def test_when_user_does_not_exist(pwd_grp):
     pwd, grp = pwd_grp
     pwd.getpwuid = MagicMock(side_effect=KeyError)
-    state.parse_args([])
-    assert state.uid == 219
-    assert state.gids == set()
+    state.state.setup_user_groups()
+    assert state.state.uid == 219
+    assert state.state.gids == set()

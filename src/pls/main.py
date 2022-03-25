1446333a3fb8f7b1956248985e425523a280ae86
Dhruv Bhanushali <dhruv_b@live.com>
2022-03-26 01:06:40 +0400

    Refactor `init` and add a dev mode

--- src/pls/main.py
+++ src/pls/main.py
@@ -1,39 +1,77 @@
 #!/usr/bin/env python3
+import logging
+import os
+
+from pls.args.parser import parser
+from pls.config import icons, prefs, specs
+from pls.config.files import find_configs
 from pls.data.utils import internal_yml_path
+from pls.globals import args, state
+from pls.log.config import configure_log_level
 
 
-def init(args=None):
+logger = logging.getLogger(__name__)
+
+
+def init(argv=None):
     """
     Initialise module variables.
+
+    :param argv: the argument vector to parse, use ``None`` to read ``sys.argv``
     """
 
-    from pls.globals import state
+    configure_log_level()
 
-    state.state = state.State()
-    state.state.parse_args(args)
-    state.state.setup()
+    logger.info("Parsing CLI arguments")
+    cli_prefs = parser.parse_args(argv)
+    logger.debug(f"CLI arguments: {cli_prefs}")
+
+    args.args.directory = cli_prefs.directory
+
+    state.state = state_obj = state.State()
+
+    state_obj.setup_home()
+    state_obj.setup_user_groups()
+    state_obj.setup_git(cli_prefs.directory)
+
+    conf_files = find_configs(cli_prefs.directory)
+    logger.debug(f"Config files read: {conf_files}")
+
+    logger.info("Reading config files")
+    conf_prefs = prefs.get_prefs(
+        [
+            *conf_files,
+            internal_yml_path("prefs.yml"),
+        ]
+    )
+    logger.debug(f"Config preferences: {conf_prefs}")
+
+    args.args.update(conf_prefs)
+    args.args.update(cli_prefs)
+
+    logger.info("Reading icons")
+    icons.nerd_icons, icons.emoji_icons = icons.get_icons(
+        [
+            *conf_files,
+            internal_yml_path("nerd_icons.yml"),
+            internal_yml_path("emoji_icons.yml"),
+        ]
+    )
+    logger.debug(f"Nerd icons count: {len(icons.nerd_icons)}")
+    logger.debug(f"Emoji icons count: {len(icons.emoji_icons)}")
+
+    logger.info("Reading node specs")
+    specs.node_specs = specs.get_specs(
+        [
+            *conf_files,
+            internal_yml_path("node_specs.yml"),
+        ]
+    )
+    logger.debug(f"Node specs count: {len(specs.node_specs)}")
 
     from pls.globals import console
 
     console.console = console.get_console()
-
-    from pls.config import icons, specs
-    from pls.config.files import find_configs
-
-    conf_files = find_configs()
-    icons.nerd_icons, icons.emoji_icons = icons.get_icons(
-        [
-            internal_yml_path("nerd_icons.yml"),
-            internal_yml_path("emoji_icons.yml"),
-            *conf_files,
-        ]
-    )
-    specs.node_specs = specs.get_specs(
-        [
-            internal_yml_path("node_specs.yml"),
-            *conf_files,
-        ]
-    )
 
 
 def main() -> None:
@@ -46,9 +84,7 @@ def main() -> None:
 
     init(None)  # ``None`` makes ``argparse`` read real CLI args from ``sys.argv``
 
-    from pls.config import specs
     from pls.fs.list import read_input
-    from pls.globals import state
     from pls.output.table import write_output
 
     node_map, node_list = read_input()
@@ -58,9 +94,9 @@ def main() -> None:
 
     for node in node_list:
         node.match_specs(specs.node_specs)
-        if state.state.collapse:
+        if args.args.collapse:
             node.find_main(node_map)
-    if state.state.collapse:
+    if args.args.collapse:
         for node in node_list:
             if node.is_sub:
                 continue
@@ -69,5 +105,7 @@ def main() -> None:
     write_output(node_list)
 
 
-if __name__ == "__main__":
+def dev() -> None:
+    os.environ.setdefault("PLS_LOG_LEVEL", "DEBUG")  # Show detailed logs
+
     main()

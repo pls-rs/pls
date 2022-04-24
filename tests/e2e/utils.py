b02044d8a437c0ff3e82adcbcef42c28689987a6
Dhruv Bhanushali <dhruv_b@live.com>
2022-04-24 15:34:11 +0400

    Provide option to extend or override arguments to `run`

--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import subprocess
 from pathlib import Path
 
@@ -9,23 +10,33 @@ from pathlib import Path
 proj_dir = Path(__file__).parents[2]
 
 
-def run_pls(args: list[str] = None) -> subprocess.CompletedProcess:
+def run_pls(pls_args: list[str] = None, **kwargs) -> subprocess.CompletedProcess:
     """
     Run ``pls`` under Poetry's virtual environment.
 
-    :param args: the list of arguments to pass to ``pls``
+    :param pls_args: the list of arguments to pass to ``pls``
+    :param kwargs: the list of arguments to pass to the ``run`` command
     :return: the ``CompletedProcess`` instance
     """
 
-    if args is None:
-        args = []
-    cmd = ["poetry", "run", "pls", *args]
-    proc = subprocess.run(
-        cmd,
-        check=True,
-        cwd=proj_dir,
-        capture_output=True,
-        text=True,
-        encoding="utf-8",
-    )
+    if pls_args is None:
+        pls_args = []
+    cmd = ["poetry", "run", "pls", *pls_args]
+
+    run_kwargs = {
+        "check": True,
+        "cwd": proj_dir,
+        "capture_output": True,
+        "text": True,
+        "encoding": "utf-8",
+    }
+
+    if "env" in kwargs:
+        env = os.environ.copy()
+        env.update(kwargs.pop("env"))
+        run_kwargs["env"] = env
+
+    run_kwargs.update(kwargs)
+
+    proc = subprocess.run(cmd, **run_kwargs)  # type: ignore
     return proc

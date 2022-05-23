6ccf19dbef09b56c2b9d28acf34aec7ca9498f4e
Dhruv Bhanushali <dhruv_b@live.com>
2022-05-23 18:36:24 +0400

    Update unit test scaffolding to support multiple working directories

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,6 +8,9 @@ scope: Literal["session"] = "session"
 
 @pytest.fixture(autouse=True, scope=scope)
 def init():
-    from pls.main import init
+    from pls.main import general_init, node_specific_init
 
-    init([])  # initialise with blank arguments
+    cli_args = general_init([])  # initialise with blank arguments
+    if len(cli_args.nodes) == 1:
+        [workdir] = cli_args.nodes
+        node_specific_init(workdir, cli_args)

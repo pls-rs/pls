6a6f6163fe05050883c3bc445eeca60543011b84
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-25 14:45:48 +0400

    Add sponsorship link in the footer

--- docs/.vuepress/components/overrides/HomeFooter.vue
+++ docs/.vuepress/components/overrides/HomeFooter.vue
@@ -15,10 +15,29 @@ const approximateStars = computed(() => Math.trunc(stars.value / 10) * 10);
 </script>
 
 <template>
-  <div class="footer">
-    <code>pls</code> is
-    <a href="https://pypi.org/project/pls/" target="_blank">free</a> and
-    <a href="https://github.com/dhruvkb/pls" target="_blank">open-source</a>
-    software. <strong>{{ approximateStars }}+ stars</strong> on GitHub.
-  </div>
+  <footer class="footer">
+    <p>
+      <code>pls</code> is
+      <a href="https://pypi.org/project/pls/" target="_blank">free</a> and
+      <a href="https://github.com/dhruvkb/pls" target="_blank">open-source</a>
+      software. <strong>{{ approximateStars }}+ stars</strong> on GitHub.
+    </p>
+    <p>
+      You can help <a href="https://github.com/sponsors/dhruvkb">sponsor</a>
+      its development.
+    </p>
+  </footer>
 </template>
+
+<style scoped lang="scss">
+footer.footer {
+  p {
+    margin-bottom: 0;
+    margin-top: 0.5em;
+
+    &:first-of-type {
+      margin-top: 0;
+    }
+  }
+}
+</style>

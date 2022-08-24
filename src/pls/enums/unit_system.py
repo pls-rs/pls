0793ce395e6f24efca5c61468e524e12aac0217c
Dhruv Bhanushali <dhruv_b@live.com>
2022-08-25 01:24:38 +0530

    Refactor `UnitSystem` enum into class-object pattern

--- src/pls/enums/unit_system.py
+++ src/pls/enums/unit_system.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 from enum import auto
+from functools import cached_property
 
 from pls.enums.base import AutoEnum
 
@@ -11,48 +12,45 @@ logger = logging.getLogger(__name__)
 
 class UnitSystem(AutoEnum):
     """
-    Bytes can be converted to higher units in two ways:
-
-    - the decimal system where powers of 10 (factor: 10^3) are used
-    - the binary system where powers of 2 (factor: 2^10) are used
-    - no system where higher units are not used
-
-    This enum lists these possibilities.
+    This enum lists the different unit systems in which measurements can be represented,
+    also including an option to not use any unit system at all.
     """
 
-    BINARY = auto()
-    DECIMAL = auto()
-    NONE = auto()
+    BINARY = auto()  # powers of 2 (steps of 2^10) are used
+    DECIMAL = auto()  # powers of 10 (steps of 10^3) are used
+    NONE = auto()  # no higher units
 
+    @cached_property
+    def base_pad_units(self) -> tuple[int, int, list[str]]:
+        """
+        Get the base factor i.e. the ratio between any two successive orders of
+        magnitude and the units corresponding to each multiplication of the base
+        factor.
 
-def get_base_and_pad_and_units(us: UnitSystem) -> tuple[int, int, list[str]]:
-    """
-    Get the base factor i.e. the ratio between any two successive orders of
-    magnitude and the units corresponding to each multiplication of the base
-    factor.
+        :return: the base factor, length of each unit string and the list of units
+        """
 
-    :param us: the unit system in use
-    :return: the base factor and the list of units
-    """
+        if self == UnitSystem.NONE:
+            return 1, 0, [""]
 
-    logger.debug(f"Unit system: {us}")
-    units = [
-        "K",  # kibi | kilo
-        "M",  # mebi | mega
-        "G",  # gibi | giga
-        "T",  # tebi | tera
-        "P",  # pebi | peta
-        "E",  # exbi | exa
-        "Z",  # zebi | zetta
-        "Y",  # yobi | yotta
-    ]
+        logger.debug(f"Unit system: {self}")
+        units = [
+            "K",  # kibi | kilo
+            "M",  # mebi | mega
+            "G",  # gibi | giga
+            "T",  # tebi | tera
+            "P",  # pebi | peta
+            "E",  # exbi | exa
+            "Z",  # zebi | zetta
+            "Y",  # yobi | yotta
+        ]
 
-    if us == UnitSystem.DECIMAL:
-        base = pow(10, 3)
-        pad = 2  # units will be 2 chars e.g. KB, GB
-        units = ["", *units]
-    else:  # us == UnitSystem.BINARY (default)
-        base = pow(2, 10)
-        pad = 3  # units will be 3 chars e.g. KiB, GiB
-        units = ["", *map(lambda i: f"{i}i", units)]
-    return base, pad, units
+        if self == UnitSystem.DECIMAL:
+            base = pow(10, 3)
+            pad = 2  # units will be 2 chars e.g. KB, GB
+            units = ["", *units]
+        else:  # self == UnitSystem.BINARY (default)
+            base = pow(2, 10)
+            pad = 3  # units will be 3 chars e.g. KiB, GiB
+            units = ["", *map(lambda i: f"{i}i", units)]
+        return base, pad, units
--- tests/unit/enums/test_unit_system.py
+++ tests/unit/enums/test_unit_system.py
@@ -1,13 +1,32 @@
-from pls.enums.unit_system import UnitSystem, get_base_and_pad_and_units
+import pytest
+
+from pls.enums.unit_system import UnitSystem
 
 
-def test_binary_system():
-    base, _, units = get_base_and_pad_and_units(UnitSystem.BINARY)
-    assert base == 1024
+@pytest.mark.parametrize(
+    "unit_system, base, pad",
+    [
+        (UnitSystem.DECIMAL, 1000, 2),
+        (UnitSystem.BINARY, 1024, 3),
+        (UnitSystem.NONE, 1, 0),
+    ],
+)
+def test_unit_system_has_correct_base_pad(unit_system: UnitSystem, base: int, pad: int):
+    us_base, us_pad, _ = unit_system.base_pad_units
+    assert base == us_base
+    assert pad == us_pad
+
+
+def test_binary_system_units_have_correct_prefix():
+    _, _, units = UnitSystem.BINARY.base_pad_units
     assert all([unit.endswith("i") for unit in units[1:]])
 
 
-def test_decimal_system():
-    base, _, units = get_base_and_pad_and_units(UnitSystem.DECIMAL)
-    assert base == 1000
+def test_decimal_system_units_have_correct_prefix():
+    _, _, units = UnitSystem.DECIMAL.base_pad_units
     assert not any([unit.endswith("i") for unit in units[1:]])
+
+
+def test_none_system_units_are_empty():
+    _, _, units = UnitSystem.NONE.base_pad_units
+    assert units == [""]

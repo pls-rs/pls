91d6b486482d0d3c0615abec972801f53e0d895a
Dhruv Bhanushali <dhruv_b@live.com>
2023-07-08 21:56:39 +0400

    Arrange constants to match order of `DetailField` enum

--- src/models/constants.rs
+++ src/models/constants.rs
@@ -2,18 +2,18 @@ use crate::enums::{DetailField, Oct, Sym, Typ};
 use std::collections::HashMap;
 
 pub struct Constants {
+	/// mapping of node type to node type info (including style)
+	pub typ: HashMap<Typ, TypInfo>,
 	/// mapping of symbolic permission bits to style
 	pub sym_styles: HashMap<Sym, String>,
 	/// mapping of octal permission bits to style
 	pub oct_styles: HashMap<Oct, String>,
-	/// style for magnitude and unit of node size
-	pub size_styles: SizeStyles,
-	/// mapping of node type to node type info (including style)
-	pub typ: HashMap<Typ, TypInfo>,
 	/// styles for the owner user
 	pub user_styles: OwnerStyles,
 	/// styles for the owner group
 	pub group_styles: OwnerStyles,
+	/// style for magnitude and unit of node size
+	pub size_styles: SizeStyles,
 	/// configuration for the table view
 	pub table: TableInfo,
 }
@@ -21,38 +21,6 @@ pub struct Constants {
 impl Default for Constants {
 	fn default() -> Self {
 		Self {
-			sym_styles: [
-				(Sym::None, "dimmed"),
-				(Sym::Read, "yellow"),
-				(Sym::Write, "red"),
-				(Sym::Execute, "green"),
-				(Sym::Special, "magenta"),
-			]
-			.into_iter()
-			.map(|(k, v)| (k, v.to_string()))
-			.collect(),
-			oct_styles: [
-				(Oct::Special, "magenta"),
-				(Oct::User, "blue"),
-				(Oct::Group, "blue dimmed"),
-				(Oct::Other, "dimmed"),
-			]
-			.into_iter()
-			.map(|(k, v)| (k, v.to_string()))
-			.collect(),
-			size_styles: SizeStyles {
-				mag: String::from("bold"),
-				prefix: String::default(),
-				base: String::from("dimmed"),
-			},
-			user_styles: OwnerStyles {
-				curr: String::from("blue bold"),
-				other: String::from("dimmed"),
-			},
-			group_styles: OwnerStyles {
-				curr: String::from("blue"),
-				other: String::from("dimmed"),
-			},
 			typ: [
 				(Typ::Dir, "d", "<dimmed>/</>", Some("dir"), "blue"),
 				(Typ::Symlink, "l", "<dimmed>@</>", Some("symlink"), ""),
@@ -76,6 +44,38 @@ impl Default for Constants {
 				)
 			})
 			.collect(),
+			sym_styles: [
+				(Sym::None, "dimmed"),
+				(Sym::Read, "yellow"),
+				(Sym::Write, "red"),
+				(Sym::Execute, "green"),
+				(Sym::Special, "magenta"),
+			]
+			.into_iter()
+			.map(|(k, v)| (k, v.to_string()))
+			.collect(),
+			oct_styles: [
+				(Oct::Special, "magenta"),
+				(Oct::User, "blue"),
+				(Oct::Group, "blue dimmed"),
+				(Oct::Other, "dimmed"),
+			]
+			.into_iter()
+			.map(|(k, v)| (k, v.to_string()))
+			.collect(),
+			user_styles: OwnerStyles {
+				curr: String::from("blue bold"),
+				other: String::from("dimmed"),
+			},
+			group_styles: OwnerStyles {
+				curr: String::from("blue"),
+				other: String::from("dimmed"),
+			},
+			size_styles: SizeStyles {
+				mag: String::from("bold"),
+				prefix: String::default(),
+				base: String::from("dimmed"),
+			},
 			table: TableInfo {
 				header_style: String::from("bold italic"),
 				column_names: [
@@ -105,22 +105,6 @@ impl Default for Constants {
 	}
 }
 
-pub struct SizeStyles {
-	/// style for the node size magnitude
-	pub mag: String,
-	/// style for the node size unit prefix
-	pub prefix: String,
-	/// style for the node size base unit
-	pub base: String,
-}
-
-pub struct OwnerStyles {
-	/// style for when the node is owned by the current user/group
-	pub curr: String,
-	/// style for when the node is owned by a different user/group
-	pub other: String,
-}
-
 pub struct TypInfo {
 	/// the character for a node type, used in the 'T' column
 	pub ch: String,
@@ -130,6 +114,22 @@ pub struct TypInfo {
 	pub icon: Option<String>, // not all node types need to have an icon
 	/// the style to use for nodes of a particular node type
 	pub style: String, // applies to name, `ch`, `suffix` and `icon`
+}
+
+pub struct OwnerStyles {
+	/// style for when the node is owned by the current user/group
+	pub curr: String,
+	/// style for when the node is owned by a different user/group
+	pub other: String,
+}
+
+pub struct SizeStyles {
+	/// style for the node size magnitude
+	pub mag: String,
+	/// style for the node size unit prefix
+	pub prefix: String,
+	/// style for the node size base unit
+	pub base: String,
 }
 
 pub struct TableInfo {

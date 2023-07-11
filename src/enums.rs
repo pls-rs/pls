faa961a51cce10f6b7ea7f36c5e2f4fe78ace686
Dhruv Bhanushali <dhruv_b@live.com>
2023-07-11 14:24:20 +0400

    Rename `sym_state` to `sym`

--- src/enums.rs
+++ src/enums.rs
@@ -3,7 +3,7 @@ mod detail_field;
 mod entity;
 mod perm;
 mod sort_field;
-mod sym_state;
+mod sym;
 mod typ;
 mod unit_sys;
 
@@ -12,6 +12,6 @@ pub use detail_field::DetailField;
 pub use entity::Entity;
 pub use perm::{Oct, Sym};
 pub use sort_field::SortField;
-pub use sym_state::SymState;
+pub use sym::SymState;
 pub use typ::Typ;
 pub use unit_sys::UnitSys;

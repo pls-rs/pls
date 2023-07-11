ed5d72fffce1515912b91c29ebd0b19f018aae7e
Dhruv Bhanushali <dhruv_b@live.com>
2023-07-11 14:25:13 +0400

    Include `Error` variant for `SymState`

--- src/enums/sym.rs
+++ src/enums/sym.rs
@@ -1,11 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-/// This enum contains the three states a symlink can be in, out of which one is
-/// fine and two are problematic.
+/// This enum contains the four states a symlink can be in, out of which one is
+/// fine and the rest are problematic.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum SymState {
 	Ok,
 	Broken,
 	Cyclic,
+	Error,
 }

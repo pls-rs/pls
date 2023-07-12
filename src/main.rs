c730c1d99622013f2678a1c93925f21230d7dacb
Dhruv Bhanushali <dhruv_b@live.com>
2023-07-12 10:34:40 +0400

    Set up the logging system

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ mod models;
 mod output;
 mod traits;
 
+use log::debug;
 use models::Pls;
 
 /// Create a `Pls` instance and immediately delegate to it.
 ///
 /// This is the entry point of the application.
 fn main() {
+	env_logger::init();
+	debug!("Hello!");
+
 	let pls = Pls::default();
 	pls.run();
+
+	debug!("Bye!");
 }

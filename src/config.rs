4ebb5ee34fa7f1f2e02bb71ff00f0513429cfbfa
Dhruv Bhanushali <dhruv_b@live.com>
2024-04-22 08:59:00 +0400

    Undo incorrect fixes

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@
 //! `pls` supports customisation in two ways, through CLI arguments that change
 //! the output per session and through `.pls.yml` YAML files that can go deeper
 //! to tweak each individual string, change icons and add new node specs.
-//! Together they make `pls` the most customizable file lister.
+//! Together they make `pls` the most customisable file lister.
 //!
 //! For example, the CLI arg `--det` controls what metadata columns must be
 //! shown in a given run, whereas the `.pls.yml` file can be used to change the

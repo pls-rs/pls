f9ba0868b133fd95d0fdf16ca067a3030ba2946c
Dhruv Bhanushali <dhruv_b@live.com>
2025-01-22 18:30:51 +0530

    Expand Prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,22 @@
+/** @type {import("prettier").Config} */
 export default {
+  trailingComma: "es5",
+  astroAllowShorthand: true,
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+  overrides: [
+    {
+      files: ["*.astro"],
+      options: {
+        parser: "astro",
+      },
+    },
+    {
+      files: ["*.svg"],
+      options: {
+        parser: "html",
+      },
+    },
+  ],
   proseWrap: "always",
 };

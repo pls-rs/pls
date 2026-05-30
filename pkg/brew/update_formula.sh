07b6cdebbb5893e6cbee17e7aec45c0105fc7ff1
Zachary Cutlip <uid000@gmail.com>
2026-05-29 18:45:10 -0700

    Build arm64 binary for Linux (#148)
    Co-authored-by: Dhruv Bhanushali <dhruv_b@live.com>

--- pkg/brew/update_formula.sh
+++ pkg/brew/update_formula.sh
@@ -13,17 +13,26 @@ curl -sL "$MAC_URL" -o /tmp/mac_asset
 MAC_SHA=$(shasum -a 256 /tmp/mac_asset | awk '{ print $1 }')
 echo "SHA256 for macOS asset is $MAC_SHA."
 
-LINUX_URL=$(echo "$RELEASE" | jq -r '.assets[] | select(.name | contains("unknown-linux-musl")).browser_download_url')
-echo "Downloading Linux asset from $LINUX_URL."
+LINUX_X86_URL=$(echo "$RELEASE" | jq -r '.assets[] | select(.name | contains("x86_64-unknown-linux-musl")).browser_download_url')
+echo "Downloading Linux x86_64 asset from $LINUX_X86_URL."
 
-curl -sL "$LINUX_URL" -o /tmp/linux_asset
-LINUX_SHA=$(shasum -a 256 /tmp/linux_asset | awk '{ print $1 }')
-echo "SHA256 for Linux asset is $LINUX_SHA."
+curl -sL "$LINUX_X86_URL" -o /tmp/linux_x86_asset
+LINUX_X86_SHA=$(shasum -a 256 /tmp/linux_x86_asset | awk '{ print $1 }')
+echo "SHA256 for Linux x86_64 asset is $LINUX_X86_SHA."
+
+LINUX_ARM_URL=$(echo "$RELEASE" | jq -r '.assets[] | select(.name | contains("aarch64-unknown-linux-musl")).browser_download_url')
+echo "Downloading Linux aarch64 asset from $LINUX_ARM_URL."
+
+curl -sL "$LINUX_ARM_URL" -o /tmp/linux_arm_asset
+LINUX_ARM_SHA=$(shasum -a 256 /tmp/linux_arm_asset | awk '{ print $1 }')
+echo "SHA256 for Linux aarch64 asset is $LINUX_ARM_SHA."
 
 sed -e "s|{{ VERSION }}|$VERSION|g" \
     -e "s|{{ MAC_URL }}|$MAC_URL|g" \
     -e "s|{{ MAC_SHA }}|$MAC_SHA|g" \
-    -e "s|{{ LINUX_URL }}|$LINUX_URL|g" \
-    -e "s|{{ LINUX_SHA }}|$LINUX_SHA|g" "$1" > "$2"
+    -e "s|{{ LINUX_X86_URL }}|$LINUX_X86_URL|g" \
+    -e "s|{{ LINUX_X86_SHA }}|$LINUX_X86_SHA|g" \
+    -e "s|{{ LINUX_ARM_URL }}|$LINUX_ARM_URL|g" \
+    -e "s|{{ LINUX_ARM_SHA }}|$LINUX_ARM_SHA|g" "$1" > "$2"
 
 echo "Formula written!"
